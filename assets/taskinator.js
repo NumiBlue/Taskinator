fa04a5c5888ed355c3fea34d91dcb6841eddee02
NumiBlue <thekindlyblog@gmail.com>
2022-01-19 13:53:44 -0600

    save task

--- assets/taskinator.js
+++ assets/taskinator.js
@@ -94,19 +94,49 @@ var taskButtonHandler = function(event) {
   var targetEl = event.target;
 
 
-  if (event.target.matches(".delete-btn")) {
-    // get the element's task id
-    var taskId = event.target.getAttribute("data-task-id");
-    console.log(taskId);
+  // edit button was clicked
+  if (targetEl.matches(".edit-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
+    editTask(taskId);
+  } 
+  // delete button was clicked
+  else if (targetEl.matches(".delete-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
     deleteTask(taskId);
   }
 };
 
+var editTask = function(taskId) {
+  console.log(taskId);
+
+
+  // get task list item element
+var taskSelected = document.querySelector(
+  ".task-item[data-task-id='" + taskId + "']"
+  );
+
+// get content from task name and type
+var taskName = taskSelected.querySelector("h3.task-name").textContent;
+console.log(taskName);
+
+var taskType = taskSelected.querySelector("span.task-type").textContent;
+console.log(taskType);
+// write values of taskName and taskType to form to be edited
+document.querySelector("input[name='task-name']").value = taskName;
+document.querySelector("select[name='task-type']").value = taskType;
+
+formEl.setAttribute("data-task-id", taskId);
+document.querySelector("#save-task").textContent = "Save Task";
+
 var deleteTask = function(taskId) {
-  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  console.log(taskId);
+  //find task list element with taskId value and remove it
+  var taskSelected = document.querySelector(
+    ".task-item[data-task-id='" + taskId + "']");
   taskSelected.remove();
+  }
 };
+ 
 
-
-
+// for edit and delete buttons
 pageContentEl.addEventListener("click", taskButtonHandler);
