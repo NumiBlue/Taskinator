9189c0ccc56764fc88502421b0230cbeceb60ed7
NumiBlue <thekindlyblog@gmail.com>
2022-01-19 11:52:44 -0600

    troubleshooting

--- assets/taskinator.js
+++ assets/taskinator.js
@@ -1,6 +1,7 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function (event) {
   event.preventDefault();
@@ -87,3 +88,23 @@ listItemEl.setAttribute("data-task-id", taskIdCounter);
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+  //get target element
+  var targetEl = event.target;
+
+  if (event.target.matches(".delete-btn")) {
+    // get the element's task id
+    var taskId = event.target.getAttribute("data-task-id");
+    console.log(taskId);
+  }
+};
+
+var deleteTask = function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
