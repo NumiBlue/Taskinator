bf9083b55df45b9d2a05fe18ad47d0656e9e8acf
NumiBlue <thekindlyblog@gmail.com>
2022-01-24 16:20:38 -0600

    vars

--- assets/taskinator.js
+++ assets/taskinator.js
@@ -1,12 +1,11 @@
 var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
-var taskStatusChangeHandler = function(event) {
+var pageContentEl = document.querySelector("#page-content");
 
-};
 
 var taskFormHandler = function (event) {
   event.preventDefault();
